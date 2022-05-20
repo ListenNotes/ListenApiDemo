116a0ed03e5cf88c12c3c36bae44f43d381c20c3
Wenbin Fang <wenbin@listennotes.com>
2022-05-20 15:40:39 -0700

    Update js dependencies.

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.less';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App tab="home" />);
+
 registerServiceWorker();
